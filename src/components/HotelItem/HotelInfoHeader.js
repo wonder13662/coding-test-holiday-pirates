feb12bacc6f6dd04ff42c0d4c9f22a19a06ccc2f
Wonder Jung <wonder13662@gmail.com>
2019-10-28 00:16:05 +0900

    Set css style to title and location, Apply unicode to stars

--- src/components/HotelItem/HotelInfoHeader.js
+++ src/components/HotelItem/HotelInfoHeader.js
@@ -1,13 +1,28 @@
 import React from "react";
+import { Header } from "semantic-ui-react";
+
+const BLACK_STAR = "\u2605";
+const WHITE_STAR = "\u2606";
 
 class HotelInfoHeader extends React.Component {
   render() {
     const { name, city, country, stars } = this.props;
+    let starArr = [WHITE_STAR, WHITE_STAR, WHITE_STAR, WHITE_STAR, WHITE_STAR];
+    for (let i = 0; i < stars; i++) {
+      starArr[i] = BLACK_STAR;
+    }
+
     return (
-      <div>
-        <div>{name}</div>
-        <div>{`${city}-${country}`}</div>
-        <div>{stars}</div>
+      <div className="hotel-info--header">
+        <div>
+          <Header as="h1">{name}</Header>
+        </div>
+        <div>
+          <Header as="h4">{`${city}-${country}`}</Header>
+        </div>
+        <div className="stars">
+          <Header as="h3">{starArr.join(" ")}</Header>
+        </div>
       </div>
     );
   }
