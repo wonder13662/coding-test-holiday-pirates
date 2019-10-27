0f46290e74590900fec9779d0d1c471b824b9b65
Wonder Jung <wonder13662@gmail.com>
2019-10-28 01:22:27 +0900

    Update layout:price, period

--- src/components/HotelItem/HotelInfoBody.js
+++ src/components/HotelItem/HotelInfoBody.js
@@ -1,4 +1,7 @@
 import React from "react";
+import moment from "moment";
+import { Header } from "semantic-ui-react";
+
 class HotelInfoBody extends React.Component {
   constructor(props) {
     super(props);
@@ -13,11 +16,17 @@ class HotelInfoBody extends React.Component {
   render() {
     const { hotelItem, toggleReviewBtn } = this.props;
     const { price, date_end, date_start } = hotelItem;
+
+    const dateStartStr = moment(date_start).format("D.M.YYYY");
+    const dateEndStr = moment(date_end).format("D.M.YYYY");
+
     return (
-      <div>
-        {toggleReviewBtn}
-        <div>{price}</div>
-        <div>{`${date_end}-${date_start}`}</div>
+      <div className="hotel-info--body">
+        <div className="toggle-btn-box">{toggleReviewBtn}</div>
+        <div className="price-n-period">
+          <div className="price">{price} &#8364;</div>
+          <div className="period">{`${dateStartStr} - ${dateEndStr}`}</div>
+        </div>
       </div>
     );
   }
